c846ea833dd3baed07fe4adcff88afeba3baed64
John Paul <johnpaultaken@gmail.com>
2018-08-14 23:39:13 -0400

    The whole async member doesn't have to be repeated.

--- thread_pool/test_thread_pool.cpp
+++ thread_pool/test_thread_pool.cpp
@@ -1,19 +1,77 @@
 #include "thread_pool.h"
 
-#include <iostream>
+#include <string>
 
 using namespace utils;
 
-int foo(double f, int i)
+int foo(double d, int i)
 {
-    return f + i;
+    std::cout << "\n static function works!";
+    return d + i;
+}
+
+class myc
+{
+public:
+    float getset(float next)
+    {
+        auto ret = current_;
+        current_ = next;
+        return ret;
+    }
+private:
+    float current_=0;
+};
+
+void copy(int & dst, int src)
+{
+    dst = src;
+}
+
+void display(std::unique_ptr<std::string> && rvalue)
+{
+    std::unique_ptr<std::string> lvalue{std::forward<std::unique_ptr<std::string>>(rvalue)};
+    std::cout << *lvalue;
 }
 
 int main()
 {
     thread_pool tp;
-    tp.async(foo, 2.3, 7);
-    tp.async([]()->void *{std::cout << "yay!"; return nullptr;});
-    tp.async([](){std::cout << "woohoo!";});
-    std::cout << "\ndone";
+    double d = 2.3;
+    int i = 7;
+    tp.async(foo, d, i);
+
+    // the following needs pointer to member specialization.
+    //myc obj;
+    //float f = 8.3f;
+    //tp.async(&myc::getset, &obj, f);
+
+    myc obj2;
+    auto stdfunc = std::bind(&myc::getset, &obj2, std::placeholders::_1);
+    float p1 = 9.4f;
+    tp.async(stdfunc, p1);
+
+    // temps not copied, but forwarded as rvalue. Hence temps cannot be used.
+    // The follows causes crash because temps dont last until async gets invoked.
+    //tp.async(std::bind(&myc::getset, &obj2, std::placeholders::_1), 9.4f);
+
+    tp.async([](){std::cout << "\n lambda works!";});
+
+    tp.async([]()->void *{std::cout << "\n lambda works with any return type!"; return nullptr;});
+
+    int dst=0;
+    int src=7;
+    tp.async(copy, dst, src);
+
+    std::unique_ptr<std::string> ps{new std::string{"\n rvalue reference forwarded"}};
+    tp.async(display, std::move(ps));
+
+    tp.wait();
+
+    std::cout << (dst==7 ? "\n lvalue reference forwarded" : "\n error: lvalue reference not forwarded");
+
+    auto obj2val = obj2.getset(0);
+    std::cout << (obj2val==9.4f ? "\n std::function works!" : "\n error: std::function doesn't work");
+
+    std::cout << "\n done";
 }
--- thread_pool/thread_pool.h
+++ thread_pool/thread_pool.h
@@ -15,37 +15,55 @@
 #include <functional>
 #include <queue>
 #include <memory>
+#include <iostream>
 
 namespace utils
 {
 
+/*
+Notes:
+1.  Use -pthread option with gcc.
+2.  Since temps are not copied but forwarded as rvalue, temps and literals
+    cannot be used as async params.
+*/
+
 class thread_pool
 {
 public:
-    template<class T>
-    using enable_if_t_void =
-        typename std::enable_if<std::is_void<T>::value, T>::type;
-
-    template<class T>
-    using enable_if_t_not_void =
-        typename std::enable_if<!std::is_void<T>::value, T>::type;
 
     template <class Fn, class... Args>
-    std::future<
-        enable_if_t_not_void<typename std::result_of<Fn(Args...)>::type>
-    >
+    static typename std::enable_if<!std::is_void<typename std::result_of<Fn(Args...)>::type>::value ,void>::type
+    run (
+        const std::shared_ptr<std::promise<typename std::result_of<Fn(Args...)>::type>> & ppromise,
+        Fn&& fn,
+        Args&&... args
+    )
+    {
+        ppromise->set_value(std::forward<Fn>(fn)(std::forward<Args>(args)...));
+    }
+
+    template <class Fn, class... Args>
+    static typename std::enable_if<std::is_void<typename std::result_of<Fn(Args...)>::type>::value ,void>::type
+    run (
+        const std::shared_ptr<std::promise<typename std::result_of<Fn(Args...)>::type>> & ppromise,
+        Fn&& fn,
+        Args&&... args
+    )
+    {
+        std::forward<Fn>(fn)(std::forward<Args>(args)...);
+        ppromise->set_value();
+    }
+
+    template <class Fn, class... Args>
+    std::future<typename std::result_of<Fn(Args...)>::type>
     async (Fn&& fn, Args&&... args)
     {
-        auto ppromise = std::make_shared<
-            std::promise<typename std::result_of<Fn(Args...)>::type>
-        >();
+        auto ppromise = std::make_shared<std::promise<typename std::result_of<Fn(Args...)>::type>>();
         q_.emplace(
             [ppromise, &fn, &args...](){
                 try
                 {
-                    ppromise->set_value(
-                        std::forward<Fn>(fn)(std::forward<Args>(args)...)
-                    );
+                    run(ppromise, std::forward<Fn>(fn), std::forward<Args>(args)...);
                 }
                 catch(std::exception &)
                 {
@@ -56,32 +74,24 @@ public:
         return ppromise->get_future();
     }
 
-    template <class Fn, class... Args>
-    std::future<enable_if_t_void<typename std::result_of<Fn(Args...)>::type>>
-    async (Fn&& fn, Args&&... args)
+    void wait()
     {
-        auto ppromise = std::make_shared<
-            std::promise<typename std::result_of<Fn(Args...)>::type>
-        >();
-        q_.emplace(
-            [ppromise, &fn, &args...](){
-                try
-                {
-                    std::forward<Fn>(fn)(std::forward<Args>(args)...);
-                    ppromise->set_value();
-                }
-                catch(std::exception &)
-                {
-                    ppromise->set_exception(std::current_exception());
-                }
+        while (! q_.empty())
+        {
+            auto & f = q_.front();
+            try
+            {
+                f();
             }
-        );
-        return ppromise->get_future();
+            catch(...)
+            {
+                std::cout << "\n Non standard exception in wait()\n";
+            }
+            q_.pop();
+        }
     }
-
 private:
     std::queue<std::function<void()>> q_;
 };
-
 
 } // namespace utils
