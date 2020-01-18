89fc89e0012895cf069694c8335f51a8ef7c215a
John Paul <johnpaultaken@gmail.com>
2020-01-18 02:13:05 -0500

    Continue Generic cache implementation.

--- cache/cache.h
+++ cache/cache.h
@@ -10,25 +10,49 @@
 
 #pragma once
 
-#include<map>
-#include<vector>
+#include <map>
+#include <vector>
+#include <chrono>
 
 namespace utils
 {
 using std::map;
 using std::vector;
+using std::chrono::system_clock;
 
 template<typename KEY, typename VAL>
 class cache
 {
+public:
     cache(size_t capacity = 1024) : _capacity(capacity)
     {
     }
+
+    // if found in cache, copies to val and return true.
+    bool find(const KEY & key, VAL & val)
+    {
+        auto itr = _lookup.find(key);
+        if (itr == _lookup.end())
+        {
+            return false;
+        }
+        else
+        {
+        }
+    }
 private:
-    size_t _capacity;
-    using value_type = struct{VAL val; size_t heap_index;};
+    using value_type = struct{
+        VAL val;
+        size_t heap_index;
+    };
     using map_type = map<KEY, value_type>;
+    using heap_type = struct{
+        system_clock::rep timestamp;
+        typename map_type::iterator itr_lookup;
+    };
+
+    size_t _capacity;
     map_type _lookup;
-    vector<struct{TIME timestamp; map_type::iterator_type itr_lookup;}> _heap;
+    vector<heap_type> _heap;
 };
 }
